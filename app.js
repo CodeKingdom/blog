2236232d58c67bf109544c50504787934882c1e4
Dan Treasure <dtreasure@getjive.com>
2014-03-04 14:25:14 -0700

    Added mongolab connection

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = function () {
 	var mongoose = require('mongoose');
 	var express = require('express');
 
-	mongoose.connect('mongodb://localhost/posts');
+	mongoose.connect('mongodb://dantreasure:yoyoman@ds033569.mongolab.com:33569/blog');
 
 	var app = express();
 	app.use(express.bodyParser());
