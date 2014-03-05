54faabf772be79f4e9bf17e9e982b194888aa105
Dan Treasure <dtreasure@getjive.com>
2014-03-04 17:58:54 -0700

    Added new routes, because I'm the shit

--- api.js
+++ api.js
@@ -1,25 +1,61 @@
 var Post = require('./post.js');
 
-var listPosts = function(req, res) {
-  Post.find()
-  .sort('title')
-  .exec(function(err, posts) {
-    res.send(posts);
-  });  
+var listPosts = function(req, res) {  
+	Post.find()
+			.sort('title')
+			.exec(function(err, posts) {
+				res.send(posts);
+			}); 
+};
+
+var getPost = function(req, res) {
+	var query = Post.findById(req.params.id);
+
+	query.exec(function(err, post){
+		if(err){
+			res.send(err);
+		}
+		res.send(post);
+	});	
 };
 
 var savePost = function(req, res) {
-  var newPost = new Post({
-    title: req.body.title,
-    body: req.body.body
-  });
-  console.log(req.body);
-  newPost.save(function(err) {
-    res.send({success: true});
-  });
+	var newPost = new Post({
+		title: req.body.title,
+		body: req.body.body
+	});
+	newPost.save(function(err) {
+		res.send({success: true});
+	});
 };
 
+var deletePost = function(req, res){
+	var query = Post.findOne({_id: req.params.id});
+
+	query.remove(function(err){
+		if(!err){
+			res.send({success:true})
+		}
+	})
+};
+
+var updatePost = function(req, res){
+	Post.findById(req.params.id, function(err, post){
+		
+		post.title = req.body.title || post.title;
+		post.body = req.body.body || post.body;
+		
+		post.save(function (err) {
+	    if (err) return (err);
+	    res.send(post);
+  	});
+	})
+}
+
 module.exports = {
-  get: listPosts,
-  post: savePost
+	get: listPosts,
+	getOne: getPost,
+	post: savePost,
+	remove: deletePost,
+	update: updatePost
 };
--- app.js
+++ app.js
@@ -9,12 +9,17 @@ var app = function () {
 
 	var api = require('./api.js');
 
+	app.get('/posts/:id', api.getOne);
+
 	app.get('/posts', api.get);
 
 	app.post('/posts', api.post);
+
+	app.delete('/posts/:id', api.remove);
+
+	app.put('/posts/:id', api.update);
 
 	return app;
 }();
 
 module.exports = app;
-
