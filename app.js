92d26b7fc4909a8c2a3ed720a8d7ea987da0546d
Dan Treasure <dtreasure@getjive.com>
2014-03-05 12:28:51 -0700

    Stopped using bodyParser, switched to use json()

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = function () {
 	mongoose.connect('mongodb://dantreasure:yoyoman@ds033569.mongolab.com:33569/blog');
 
 	var app = express();
-	app.use(express.bodyParser());
+	app.use(express.json());
 
 	var post = require('./api/post.js');
 
