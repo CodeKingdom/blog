be5056d7c50921914d3489905b7c0410abe9bf84
Wesley Smith <wesleyfsmith@gmail.com>
2014-03-19 14:33:35 -0600

    Allowed server to do CORS requests.

--- app.js
+++ app.js
@@ -11,27 +11,42 @@ var app = function () {
 
 	var user = require('./api/user.js');
 
-	app.get('/posts/:id', post.getOne);
+    //this pattern enables cross domain requests
+    app.all('*', function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        next();
+    });
 
-	app.get('/posts', post.get);
+    app.get('/', function(req, res, next) {
+        app.get('/posts', post.get);
 
-	app.post('/posts', post.post);
+        app.get('/posts/:id', post.getOne);
 
-	app.delete('/posts/:id', post.remove);
+        app.get('/users/:id', user.getOne);
 
-	app.put('/posts/:id', post.update);
+        app.get('/users', user.get);
 
-	app.get('/users/:id', user.getOne);
+        app.get('/users/:id/posts', user.getPosts);
+    });
 
-	app.get('/users', user.get);
+    app.post('/', function(req, res, next) {
+        app.post('/posts', post.post);
 
-	app.post('/users', user.post);
+        app.post('/users', user.post);
+    });
 
-	app.delete('/users/:id', user.remove);
+    app.delete('/', function(req, res, next) {
+        app.delete('/posts/:id', post.remove);
 
-	app.put('/users/:id', user.update);
+        app.delete('/users/:id', user.remove);
+    });
 
-	app.get('/users/:id/posts', user.getPosts);
+    app.delete('/', function(req, res, next) {
+        app.put('/posts/:id', post.update);
+
+        app.put('/users/:id', user.update);
+    });
 
 	return app;
 }();
