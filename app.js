088bddab05cbd0e339417a70196138c5d977db8f
Dan Treasure <dtreasure@getjive.com>
2014-03-25 12:03:37 -0600

    Fixes to app.js

--- app.js
+++ app.js
@@ -7,46 +7,42 @@ var app = function () {
 	var app = express();
 	app.use(express.bodyParser());
 
+
 	var post = require('./api/post.js');
 
 	var user = require('./api/user.js');
 
     //this pattern enables cross domain requests
     app.all('*', function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "X-Requested-With");
         next();
     });
 
-    app.get('/', function(req, res, next) {
-        app.get('/posts', post.get);
+	app.get('/posts', post.get);
 
-        app.get('/posts/:id', post.getOne);
+	app.get('/posts/:id', post.getOne);
 
-        app.get('/users/:id', user.getOne);
+	app.get('/users/:id', user.getOne);
 
-        app.get('/users', user.get);
+	app.get('/users', user.get);
 
-        app.get('/users/:id/posts', user.getPosts);
-    });
+	app.get('/users/:id/posts', user.getPosts);
 
-    app.post('/', function(req, res, next) {
-        app.post('/posts', post.post);
 
-        app.post('/users', user.post);
-    });
+	app.post('/posts', post.post);
 
-    app.delete('/', function(req, res, next) {
-        app.delete('/posts/:id', post.remove);
+	app.post('/users', user.post);
 
-        app.delete('/users/:id', user.remove);
-    });
 
-    app.delete('/', function(req, res, next) {
-        app.put('/posts/:id', post.update);
+	app.delete('/posts/:id', post.remove);
 
-        app.put('/users/:id', user.update);
-    });
+	app.delete('/users/:id', user.remove);
+
+
+	app.put('/posts/:id', post.update);
+
+	app.put('/users/:id', user.update);
 
 	return app;
 }();
